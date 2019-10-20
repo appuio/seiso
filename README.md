66418e1c5bf8742f88de4614bee5b3860c37788a
Tobias Nehrlich <tobias.nehrlich@vshn.ch>
2019-10-20 10:49:27 +0200

    Add Cobra CLI helper

--- README.md
+++ README.md
@@ -1,9 +1,16 @@
 # Image Cleanup Client
 
 ## Dependencies
-* github.com/openshift/client-go to interact with OpenShift clusters
-* github.com/heroku/docker-registry-client to interact with Docker registries
-* `flag` for CLI flags
+
+|Name|Usage|
+|---|---|
+|[openshift/client-go](https://github.com/openshift/client-go)|Interaction with OpenShift clusters|
+|[heroku/docker-registry-client](https://github.com/heroku/docker-registry-client)|Interaction with Docker registries|
+|[spf13/cobra](https://github.com/spf13/cobra)|CLI helper|
+|[src-d/go-git](https://github.com/src-d/go-git)|Interaction with git repositories|
+|||
+
+The respective licenses for attribution are placed in `/attribution`.
 
 ## Development
 Build and run:
--- main.go
+++ main.go
@@ -1,72 +1,7 @@
 package main
 
-import (
-	"flag"
-	"fmt"
-
-	imagev1client "github.com/openshift/client-go/image/clientset/versioned/typed/image/v1"
-
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-
-	"k8s.io/client-go/tools/clientcmd"
-
-	"github.com/heroku/docker-registry-client/registry"
-)
+import "github.com/appuio/image-cleanup-client/commands"
 
 func main() {
-	// Instantiate loader for kubeconfig file.
-	kubeconfig := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(
-		clientcmd.NewDefaultClientConfigLoadingRules(),
-		&clientcmd.ConfigOverrides{},
-	)
-
-	namespace := resolveNamespace(kubeconfig)
-
-	flag.StringVar(&namespace, "n", namespace, "Namespace to use, defaults to the namespace defined in kubeconfig")
-	flag.Parse()
-
-	// Get a rest.Config from the kubeconfig file.  This will be passed into all
-	// the client objects we create.
-	restconfig, err := kubeconfig.ClientConfig()
-	if err != nil {
-		panic(err)
-	}
-
-	// Create an OpenShift image/v1 client.
-	imageclient, err := imagev1client.NewForConfig(restconfig)
-	if err != nil {
-		panic(err)
-	}
-
-	imagestreamlist, err := imageclient.ImageStreams(namespace).List(metav1.ListOptions{})
-	if err != nil {
-		panic(err)
-	}
-
-	for _, imagestream := range imagestreamlist.Items {
-		fmt.Println(imagestream.ObjectMeta.Name)
-	}
-
-	url := "https://registry-1.docker.io/"
-	username := "" // anonymous
-	password := "" // anonymous
-	hub, err := registry.New(url, username, password)
-
-	tags, err := hub.Tags("appuio/oc")
-	if err != nil {
-		panic(err)
-	}
-
-	for _, tag := range tags {
-		fmt.Println(tag)
-	}
-}
-
-// Get the namespace defined in the kubeconfig
-func resolveNamespace(kubeconfig clientcmd.ClientConfig) (namespace string) {
-	namespace, _, err := kubeconfig.Namespace()
-	if err != nil {
-		panic(err)
-	}
-	return
+	commands.Execute()
 }
